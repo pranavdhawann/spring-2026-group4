57790d235e541e2f687cf8f4c3454681fcec2f62
Aakash Singh <aakashsingh717@gmail.com>
2026-02-13 13:55:31 -0500

    file rename and some error handling cases in create_tabular_list.py

--- setupScripts/create_tabular_list.py
+++ setupScripts/create_tabular_list.py
@@ -1,29 +1,23 @@
-import os
 import csv
-import yaml
+import os
+
+from src.utils import read_yaml
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.dirname(script_dir)
 config_path = os.path.join(project_root, "config", "config.yaml")
 
-def load_config(path):
-    with open(path, 'r') as file:
-        return yaml.safe_load(file)
 
 def create_ticker_csv():
-    if not os.path.exists(config_path):
-        print(f"Error: Config file not found at {config_path}")
-        return
+    config = read_yaml(config_path)
 
-    config = load_config(config_path)
-    
-    tabular_data_rel_path = config.get('TABULAR_DATA')
+    tabular_data_rel_path = config.get("TABULAR_DATA")
     if not tabular_data_rel_path:
         print("Error: TABULAR_DATA not found in config.")
         return
 
     sp500_table_dir = os.path.join(project_root, tabular_data_rel_path)
-    
+
     output_dir = os.path.dirname(sp500_table_dir)
     output_csv = os.path.join(output_dir, "table_tickers.csv")
 
@@ -32,21 +26,28 @@ def create_ticker_csv():
         print(f"Error: Directory {sp500_table_dir} does not exist.")
         return
 
-    tickers = [name for name in os.listdir(sp500_table_dir) if os.path.isdir(os.path.join(sp500_table_dir, name))]
-    
-    tickers.sort()
+    tickers = [
+        name
+        for name in os.listdir(sp500_table_dir)
+        if os.path.isdir(os.path.join(sp500_table_dir, name))
+    ]
 
+    tickers.sort()
+    print(tickers[0])
+    if tickers[0] == "TRUE":
+        tickers = tickers[1:]
     print(f"Found {len(tickers)} tickers.")
 
     try:
-        with open(output_csv, mode='w', newline='', encoding='utf-8') as csvfile:
+        with open(output_csv, mode="w", newline="", encoding="utf-8") as csvfile:
             writer = csv.writer(csvfile)
-            writer.writerow(['ticker'])  
+            writer.writerow(["ticker"])
             for ticker in tickers:
                 writer.writerow([ticker])
         print(f"Successfully created {output_csv}")
     except Exception as e:
         print(f"Error writing to CSV: {e}")
+
 
 if __name__ == "__main__":
     create_ticker_csv()
