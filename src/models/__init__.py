aa6c0e092177428a354a5d43e18faeef734cdf25
Aakash Singh <aakashsingh717@gmail.com>
2026-02-24 19:54:44 -0500

    added train_baseline_multimodal_1.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,11 @@
 from .finBert import FinBERTModel
 from .finBertForecastingBaseline import FinBertForecastingBL
+from .multiModalBaseline import MultiModalStockPredictor
 from .timeseries_lstm import LSTMForecaster
 
-__all__ = ["FinBERTModel", "FinBertForecastingBL", "LSTMForecaster"]
+__all__ = [
+    "FinBERTModel",
+    "FinBertForecastingBL",
+    "LSTMForecaster",
+    "MultiModalStockPredictor",
+]
